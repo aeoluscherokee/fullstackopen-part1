3b5d976de697811b31de366898677c904330e3f2
Krittanu Royapan <krittanu.riyapan@gmail.com>
2021-11-03 00:23:40 +0700

    Change course and parts into single object

--- courseinfo/src/App.js
+++ courseinfo/src/App.js
@@ -9,7 +9,11 @@ const Header = (props) => {
 };
 
 const Part = (props) => {
-  return <p>{props.name} {props.exercises}</p>;
+  return (
+    <p>
+      {props.name} {props.exercises}
+    </p>
+  );
 };
 
 const Content = (props) => {
@@ -38,21 +42,23 @@ const Total = (props) => {
 };
 
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
