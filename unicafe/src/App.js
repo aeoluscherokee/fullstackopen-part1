d28fe1e52fd2168f6fe2d8229c0f1c2084ae24ac
Krittanu Royapan <krittanu.riyapan@gmail.com>
2021-11-03 14:47:01 +0700

    Draw template for app

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button>good</button>
+      <button>neutral</button>
+      <button>bad</button>
+      <h1>statistics</h1>
+      <p>good 6</p>
+      <p>neutral 2</p>
+      <p>bad 1</p>
+    </div>
+  );
 };
 
 export default App;
