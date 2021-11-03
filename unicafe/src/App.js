98ea18f4c3ca6c467970a3b28b7bf12127be2a15
Krittanu Royapan <krittanu.riyapan@gmail.com>
2021-11-03 14:53:56 +0700

    Use useState to display feedback statistic

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Button = ({ name }) => {
   return <button>{name}</button>;
 };
 
 const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
   return (
     <div>
       <h1>give feedback</h1>
@@ -12,9 +16,9 @@ const App = () => {
       <Button name="neutral" />
       <Button name="bad" />
       <h1>statistics</h1>
-      <p>good 6</p>
-      <p>neutral 2</p>
-      <p>bad 1</p>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
     </div>
   );
 };
