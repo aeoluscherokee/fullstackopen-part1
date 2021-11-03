2ac4081bdeb3f0cc0e59249031ded655a41d9248
Krittanu Royapan <krittanu.riyapan@gmail.com>
2021-11-03 14:49:15 +0700

    Create Button component

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 
+const Button = ({ name }) => {
+  return <button>{name}</button>;
+};
+
 const App = () => {
   return (
     <div>
       <h1>give feedback</h1>
-      <button>good</button>
-      <button>neutral</button>
-      <button>bad</button>
+      <Button name="good" />
+      <Button name="neutral" />
+      <Button name="bad" />
       <h1>statistics</h1>
       <p>good 6</p>
       <p>neutral 2</p>
